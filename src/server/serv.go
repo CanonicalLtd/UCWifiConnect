59b6372da5760113d88cd1f318d35ff0b83f3695
kyle.nitzsche@canonical.com <kyle.nitzsche@canonical.com>
2017-03-08 19:49:12 +0000

    cleaner serv.go html output

--- src/server/serv.go
+++ src/server/serv.go
@@ -3,19 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/CanonicalLtd/UCWifiConnect/netman"
 )
-
-func contains(s []string, e string) bool {
-    for _, a := range s {
-        if a == e {
-            return true
-        }
-    }
-    return false
-}
 
 func para(s string) string {
 	return fmt.Sprintf("<p>%s</p>", s)
@@ -33,15 +23,12 @@ func form(SSIDs []netman.SSID) string {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	d, _ := os.Getwd()
-	//fmt.Fprintf(w, "<p>URL path: %s</p>", r.URL.Path[1:])
-	fmt.Fprintf(w, "<p>pwd: %s", d)
+	
+	fmt.Fprintf(w, "<html><head></head></body>")
 	ssids, _, _ := netman.Ssids()
 	ssids_form := form(ssids)
 	fmt.Fprintf(w, ssids_form)
-	//for _, s := range getWifi() {
-	//	fmt.Fprintf(w, para(s))
-	//}
+	fmt.Fprintf(w, "</html>")
 }
 
 func main() {
