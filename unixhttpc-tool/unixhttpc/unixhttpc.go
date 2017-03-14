9e6765e38bed4af100077c25c8ba4d093b32a3bd
Roberto Mier Escandon <rmescandon@gmail.com>
2017-03-14 16:15:04 +0100

    first version workin in 1.6

--- unixhttpc-tool/unixhttpc/unixhttpc.go
+++ unixhttpc-tool/unixhttpc/unixhttpc.go
@@ -1,11 +1,12 @@
-package main
+/*package main
 
 import (
 	"fmt"
 	"io"
+	"log"
+	"net"
 	"net/http"
 	"os"
-	"time"
 )
 
 func main() {
@@ -15,13 +16,74 @@ func main() {
 	}
 	fmt.Println("Unix HTTP client")
 
-	httpc := http.Client{
-		Timeout: time.Second * 5,
+	req, err := http.NewRequest("GET", os.Args[3], nil)
+	if err != nil {
+		log.Fatal("New request error", err)
 	}
 
-	response, err := httpc.Get("http://unix" + os.Args[2])
-	if err != nil {
-		panic(err)
+	var resp *http.Response
+
+	httpc := &http.Client{
+		Transport: &http.Transport{
+			Dial: net.Dial("unix", os.Args[1]),
+		},
 	}
-	io.Copy(os.Stdout, response.Body)
+	resp, err = httpc.Do(req)
+	if err != nil {
+		log.Fatal("Do request error", err)
+	}
+
+	defer resp.Body.Close()
+
+	io.Copy(os.Stdout, resp.Body)
+}*/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"time"
+)
+
+func reader(r io.Reader) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if err != nil {
+			return
+		}
+		println("Client got:", string(buf[0:n]))
+
+		//io.Copy(os.Stdout, response.Body)
+	}
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "usage:", os.Args[0], "/path.socket /uri")
+		return
+	}
+	fmt.Println("Unix HTTP client.")
+
+	c, err := net.Dial("unix", os.Args[1])
+	if err != nil {
+		fmt.Println("Dial error: ", err)
+		return
+	}
+	defer c.Close()
+
+	go reader(c)
+
+	requestMsg := "GET http://unix" + os.Args[2] + " HTTP/1.1\r\n" +
+		"Host: localhost\r\n" +
+		"\r\n"
+
+	_, err = c.Write([]byte(requestMsg))
+	if err != nil {
+		fmt.Println("write error: ", err)
+		return
+	}
 }
