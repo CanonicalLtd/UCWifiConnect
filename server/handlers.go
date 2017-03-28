876d24cc8317a255f0b8245f74f5f78cb5592737
Roberto Mier Escandon <rmescandon@gmail.com>
2017-03-28 09:59:09 +0200

    added alert box. Updated wifis and connect handlers

--- server/handlers.go
+++ server/handlers.go
@@ -17,17 +17,39 @@ const (
 // ResourcesPath absolute path to web static resources
 var ResourcesPath = filepath.Join(os.Getenv("SNAP"), "static")
 
+// Network entry for a wifi network in page data
+type Network struct {
+	Ssid   string
+	Status string
+}
+
 // PageData dynamic data to fulfill the template
 type PageData struct {
-	Ssids []netman.SSID
+	Networks []Network
+	Alert    string
 }
 
 // SsidsHandler lists the current available SSIDs
 func SsidsHandler(w http.ResponseWriter, r *http.Request) {
 
+	var connectedWifi = ""
+	if netman.ConnectedWifi() {
+		//TODO get here the network we are connected to
+		connectedWifi = "<TheWifiWeAreConnectedTo>"
+	}
+
 	// build dynamic data object
 	ssids, _, _ := netman.Ssids()
-	data := PageData{ssids}
+	networks := make([]Network, len(ssids))
+	for i := range ssids {
+		status := "disconnected"
+		if ssids[i].Ssid == connectedWifi {
+			status = "connected"
+		}
+		networks[i] = Network{Ssid: ssids[i].Ssid, Status: status}
+	}
+
+	data := PageData{Networks: networks}
 
 	// parse template
 	templateAbsPath := filepath.Join(ResourcesPath, templatePath)
@@ -44,4 +66,32 @@ func SsidsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+// ConnectHandler reads form got ssid and password and tries to connect to that network
+func ConnectHandler(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+
+	ssids := r.Form["ssid"]
+	if len(ssids) == 0 {
+		log.Println("SSID not provided")
+		return
+	}
+	ssid := ssids[0]
+
+	pwd := ""
+	pwds := r.Form["pwd"]
+	if len(pwds) > 0 {
+		pwd = pwds[0]
+	}
+
+	log.Printf("Connecting to %v...", ssid)
+
+	//connect
+	_, ap2device, ssid2ap := netman.Ssids()
+	netman.ConnectAp(ssid, pwd, ap2device, ssid2ap)
+
+	// redirect to list
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
