3e2caa308dc37b69bcfd3280da0431b4f8b18443
kyle.nitzsche@canonical.com <kyle.nitzsche@canonical.com>
2017-03-31 17:52:12 +0000

    Further refinement of daemon, but still incomplete
    On starting it waits ten seconds (proper time TBD) to allow wifi
    connection, if any. Then loops. On each iter:
    
    if there are no netmanager reported wifi connections, wlan0 is set
    to managed by NM (if it is not already), then ssids are retrieved
    and if any found, we will (but don't yet) start Management mode
    http server (if not running) and bring up the wifi-ap, else we
    loop until we get ssids.
    
    If on an iter wifi is connected, we will (but don't yet) set to
    Operational mode by starting the Opertaional mode http server (if
    not already running).
    
    Testable now. After connecting the interfaces, you can connect to
    external wifi with:
    $ wifi-connect.netman, after which you see this
    in journal every five seconds:
    
    ==== Start Operational Mode http server...
    
    Then disconnect from external wifi with:
    $ wifi-connect.netman-disconnect-wifi
    after which you see this every five seconds
    
    ==== No network manager wifi connection
    ==== Start Management Mode http server...
    ==== Start wifi-ap AP...
    
    Current impl needs following change: when no wifi connection, so
    Management mode, first check if we have any known ssids. if yes,
    check if wifi-AP is UP, and if not, put it UP. If no known ssids,
    bring wifi-AP down, set wlan0 MANAGED, and get SSIDS (so on future
    loop iter the IP is put UP.)

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -10,22 +10,63 @@ import (
 	"github.com/CanonicalLtd/UCWifiConnect/netman"
 )
 
-func main() {
-	c := netman.DefaultClient()
+func checkSsids(c *netman.Client) bool {
 	ssidsFile := os.Getenv("SNAP_COMMON") + "/ssids"
+	SSIDs, _, _ := c.Ssids()
+	//only write SSIDs when found
+	if len(SSIDs) > 0 {
+		var out string
+		for _, ssid := range SSIDs {
+			out += strings.TrimSpace(ssid.Ssid) + ","
+		}
+		out = out[:len(out)-1]
+		err := ioutil.WriteFile(ssidsFile, []byte(out), 0644)
+		if err != nil {
+			fmt.Println("Error writing ssids to ", ssidsFile)
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	first := true
+	c := netman.DefaultClient()
+	// add code
+
 	for {
-		SSIDs, _, _ := c.Ssids()
-		//only write SSIDs when found
-		if len(SSIDs) > 0 {
-			var out string
-			for _, ssid := range SSIDs {
-				out += strings.TrimSpace(ssid.Ssid) + ","
+		if first {
+			first = false
+			//wait one minute on first run to allow wifi connections
+			//TODO: time period to be refined
+			time.Sleep(10000 * time.Millisecond)
+			//time.Sleep(60000 * time.Millisecond)
+		}
+		//if not connected to external wifi, set to Management Mode
+		if !c.ConnectedWifi(c.GetWifiDevices(c.GetDevices())) {
+			fmt.Println("==== No network manager wifi connection")
+			// if wlan0 not managed by network manager, set to managed
+			wifis, _ := c.WifisManaged(c.GetWifiDevices(c.GetDevices()))
+			found := false
+			for k, _ := range wifis {
+				if k == "wlan0" {
+					found = true
+				}
 			}
-			out = out[:len(out)-1]
-			err := ioutil.WriteFile(ssidsFile, []byte(out), 0644)
-			if err != nil {
-				fmt.Println("Error writing ssids to ", ssidsFile)
+			if !found {
+				c.SetIfaceManaged("wlan0", c.GetWifiDevices(c.GetDevices()))
 			}
+			// check for ssids, if any found, take steps
+			if checkSsids(c) {
+				//TODO: start Management Mode http server. requires new pkg funcs
+				fmt.Println("==== Start Management Mode http server...")
+				//TODO: start wifi-ap
+				fmt.Println("==== Start wifi-ap AP...")
+			}
+		} else { //Set to operational mode
+			//TODO create operational mode server
+			fmt.Println("==== Start Operational Mode http server...")
 		}
 		// wait 5 seconds
 		time.Sleep(5000 * time.Millisecond)
