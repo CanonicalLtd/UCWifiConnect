20568e52a38984b6a3b1341c803bdf9e5c32e2b4
kyle.nitzsche@canonical.com <kyle.nitzsche@canonical.com>
2017-04-04 22:29:50 +0000

    netman add SetIfaceUnmanaged()

--- cmds/netman/cmd_netman.go
+++ cmds/netman/cmd_netman.go
@@ -18,6 +18,7 @@ type options struct {
 	disconnectWifi     bool
 	wifisManaged       bool
 	setIfaceManaged    string
+	setIfaceUnmanaged  string
 }
 
 func args() *options {
@@ -29,6 +30,7 @@ func args() *options {
 	flag.BoolVar(&opts.disconnectWifi, "disconnect-wifi", false, "Disconnect from any and all external wifi")
 	flag.BoolVar(&opts.wifisManaged, "wifis-managed", false, "Show list of wifi interfaces that are managed by network-manager")
 	flag.StringVar(&opts.setIfaceManaged, "manage-iface", "", "Set the specified interface to be managed by network-manager.")
+	flag.StringVar(&opts.setIfaceUnmanaged, "unmanage-iface", "", "Set the specified interface to NOT be managed by network-manager.")
 	flag.Parse()
 	return opts
 }
@@ -81,6 +83,10 @@ func main() {
 		c.SetIfaceManaged(opts.setIfaceManaged, c.GetWifiDevices(c.GetDevices()))
 		return
 	}
+	if len(opts.setIfaceUnmanaged) > 0 {
+		c.SetIfaceUnmanaged(opts.setIfaceUnmanaged, c.GetWifiDevices(c.GetDevices()))
+		return
+	}
 	if opts.wifisManaged {
 		wifis, err := c.WifisManaged(c.GetWifiDevices(c.GetDevices()))
 		if err != nil {
@@ -92,6 +98,7 @@ func main() {
 		}
 		return
 	}
+
 	SSIDs, ap2device, ssid2ap := c.Ssids()
 
 	for _, ssid := range SSIDs {
