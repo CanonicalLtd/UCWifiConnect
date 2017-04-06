6b2add18dc63add8276502de7bfdbfa813c50239
kyle.nitzsche@canonical.com <kyle.nitzsche@canonical.com>
2017-04-06 20:07:07 +0000

    Revert launcher.go, which I did not mean to change

--- server/launcher.go
+++ server/launcher.go
@@ -9,59 +9,11 @@ import (
 	"log"
 )
 
-const (
-	address = ":8080"
-)
-
-// ListenAndServe starts http server.
-
-// StartManagementServer starts web server for AP in management mode
-/* Howto start and stop server
-srvCLoser, err := server.StartManagementServer()
-if err != nil {
-	log.Fatalln("StartManagementServer Error - ", err)
-}
-
-// Do Stuff
-
-// Close HTTP Server
-err = srvCLoser.Close()
-if err != nil {
-	log.Fatalln("Server Close Error - ", err)
-}
-
-log.Println("Server Closed")
-*/
-func StartManagementServer() (sc io.Closer, err error) {
-	return listenAndServe(address, managementHandler())
-}
-
-// StartExternalServer starts web server when connected to external WIFI
-/* Howto start and stop server
-srvCLoser, err := server.StartExternalServer()
-if err != nil {
-	log.Fatalln("StartExternalServer Error - ", err)
-}
-
-// Do Stuff
-
-// Close HTTP Server
-err = srvCLoser.Close()
-if err != nil {
-	log.Fatalln("Server Close Error - ", err)
-}
-
-log.Println("Server Closed")
-*/
-func StartExternalServer() (sc io.Closer, err error) {
-	return listenAndServe(address, externalHandler())
-}
-
 type tcpKeepAliveListener struct {
 	*net.TCPListener
 }
 
-// Accept accepts incomming tcp connections
+// Accept accepts incoming tcp connections
 func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
 	tc, err := ln.AcceptTCP()
 	if err != nil {
